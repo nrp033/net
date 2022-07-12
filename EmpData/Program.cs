6f3927cc4f587ef25aab3dac1fbd9a29c2851616
Nitin Pawar <pawarnitin9433@gmail.com>
2022-07-12 18:44:09 +0530

    Employee Manegement

---
+++ EmpData/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EmpMgmt
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            presentee.emp_presentee();
+            wagesonly.wagecal();
+            Part_time.Parttime();
+            Emp_wage_Case.Emp_Case();
+           
+           
+        }
+    }
+}
---
+++ EmpData/presentee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpMgmt
+{
+    public class presentee
+    {
+        //Program to check if Employee is Present or Absent.
+
+       public static void emp_presentee()
+        {
+            int is_present = 1;
+            Random rd = new Random();
+            int echeck = rd.Next(0,2);
+            if (echeck == is_present)
+            {
+                Console.WriteLine("Employee is Present.") ;
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+
+            }
+        }
+    }
+}
